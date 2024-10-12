e9c747acece31d27dc476d0c886944e68361bd3b
yunjii <162238479+itsmeyji@users.noreply.github.com>
2024-10-13 02:56:45 +0900

    [Bronze V] Title: A×B, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner come = new Scanner(System.in);
+        int A = come.nextInt();
+        int B = come.nextInt();
+        System.out.println(A*B);
+    }
+}
