a679d2da1b04b5b3163f4842fe1eef06d815a5cd
yunjii <162238479+itsmeyji@users.noreply.github.com>
2024-10-13 03:19:54 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        name = name + "??!";
+        System.out.println(name);
+    }
+}
